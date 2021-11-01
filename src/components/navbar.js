451f34eb16bbd3c66058a1ebb622941fa1211479
Agustina Dolar <agustinadolar@gmail.com>
2021-11-01 13:04:09 -0300

    Mensaje para el commit

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-dark bg-dark">
+                <ul>
+                    <li>Inicio</li>
+                    <li>Productos</li>
+                    <li>Contacto</li>
+                    <li>Quienes somos</li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
