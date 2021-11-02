b6939d5a9c365b043c55a01b57afaa8f8c768eec
Agustina Dolar <agustinadolar@gmail.com>
2021-11-02 20:40:16 -0300

    landingg

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Productodestacado from './components/Productodestacado';
+import Itemlistcontainer from './components/Itemlistcontainer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <h1 className="App">Bienvenidos a</h1>
     <h2 className="App">Galeria virtual</h2>
     <Navbar/>
+    <Itemlistcontainer/>
     <h3>Productos Destacados</h3>
     <hr />
     < Productodestacado nombre="Dama del armiño" que="Recreacion La Dama del armiño de Leonardo Da vinci" autor="Gonzalo Dolar" />
---
+++ src/components/Itemlistcontainer.js
@@ -0,0 +1,9 @@
+function Itemlistcontainer () {
+    return(
+    <>
+
+    </>
+    );
+}
+
+export default Itemlistcontainer;
